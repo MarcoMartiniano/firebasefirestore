66682f0b48060c466037f23729c47a1af5baf624
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-06-11 13:52:32 -0300

    primeiro commit
    todos arquivos

---
+++ PortifolioFireBaseFirestore/app/src/main/java/com/marco/portifoliofirebasefirestore/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.marco.portifoliofirebasefirestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnUploadData.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val person = Person(firstName,lastName,age)
+            savePerson(person)
+        }
+
+       // subscribetoRealtimeUpdates()
+
+
+        btnRetrieveData.setOnClickListener {
+            retrievePersons()
+        }
+
+
+    }
+
+
+
+    private fun subscribetoRealtimeUpdates(){
+        personCollectionRef.addSnapshotListener{querySnapshot, firebaseFirestoreException ->
+        firebaseFirestoreException?.let {
+            Toast.makeText(this,it.message,Toast.LENGTH_LONG).show()
+            return@addSnapshotListener
+        }
+            querySnapshot?.let {
+                val sb = StringBuilder()
+                for(document in it){
+                    val person = document.toObject<Person>()
+                    sb.append("$person\n")
+                }
+                tvPersons.text = sb.toString()
+            }
+        }
+    }
+
+
+
+    private fun retrievePersons() = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val querySnapshot = personCollectionRef.get().await()
+            val sb = StringBuilder()
+            for(document in querySnapshot.documents){
+                val person = document.toObject<Person>()
+                sb.append("$person\n")
+            }
+            withContext(Dispatchers.Main){
+                tvPersons.text = sb.toString()
+            }
+
+        }catch (e:Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+
+
+    private fun savePerson (person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity,"Successfully saved data.",Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
